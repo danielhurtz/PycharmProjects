36f9d6f2036a2a42daa0027b3d1f852c56e521c3
Daniel Oliveira <daniel.anjos@protonmail.com>
2022-05-27 16:03:25 -0300

    up3

---
+++ pythonteste/teste10.py
@@ -0,0 +1,10 @@
+frase = '  Curso em Video Python  '
+print(len(frase.strip())) # remove os espacos antes e depois da frase
+print(len(frase)) # mostra o total de letras / tamanho da frase
+print(frase.upper().count('O'))
+
+print("""Welcome! Are you completely new to programming?
+about why and how get started with Python. Fortunately
+an experienced programmer in any programming language
+(whatever it may be) can pick uup Python very quickly.
+Its also easy for beginners to use and learn, so jump in!""")
---
+++ pythonteste/teste10a.py
@@ -0,0 +1,4 @@
+frase = 'Curso em Video Python'
+print(frase.lower().find('video'))
+print(frase.replace('Python', 'Android'))
+print(frase)
